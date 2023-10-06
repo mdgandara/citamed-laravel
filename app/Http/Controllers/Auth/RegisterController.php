326ab9131cada60e8a878d12370c8bb54c1c7bd4
mdgandara <mdgandara@soy.sena.edu.co>
2023-10-05 19:42:15 -0500

    punto de control

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'documentoId' => ['required', 'numeric','unique:users','confirmed'],
+            'documentoId' => ['required', 'numeric','unique:users'],
             'name' => ['required', 'string', 'max:255'],
             'surname' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
